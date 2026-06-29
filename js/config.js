8bc73d47fba7f6db64fc359c1bee709f8a9fefaf
mangueiracontas-del <mangueira.contas@gmail.com>
2026-06-29 12:12:53 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,8 +8,10 @@
 // 2. Vá em Settings > API
 // 3. Copie a "Project URL" e a "anon public" key
 //
-
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://llnecvfzboqqgspvbibg.supabase.co'
 const supabaseKey = 'sb_publishable_b8LkKFq6_pKoMsPbXzguoA_VAbpZIT4'
+// Define variáveis globais para uso em outros arquivos
+window.SUPABASE_URL = supabaseUrl
+window.SUPABASE_ANON = supabaseKey
 const supabase = createClient(supabaseUrl, supabaseKey)
